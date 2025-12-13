71e5e6991fd5f3d04d4c2594884dd4e616727fb8
Claudia Hernandez <claudia.hdez.perez.2004@gmail.com>
2025-12-12 21:14:46 -0500

    fix/bug in partition

--- src/identity-manager/app/models/state.py
+++ src/identity-manager/app/models/state.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 class State(BaseModel):
     current_term: int = 0
     voted_for: str | None = None
+    current_leader: str | None = None
     commit_index: int = -1
     last_applied: int = -1
 
